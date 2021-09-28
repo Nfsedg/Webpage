a7be26cc162f5b93fd351eb71521e42cb8c09be5
Edgar Pech <edgaropech@hotmail.com>
2021-09-28 17:19:34 -0500

    nav logo and selfphoto (aboutme) added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,54 @@
 const { request } = require('http')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: "production"
+    entry: "./docs/scripts/queryjs.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          },
+          { 
+            test: /\.css$/, 
+            loader: [
+              MiniCSSExtractPlugin.loader,
+              "css-loader"
+            ]
+          },
+          { 
+            test: /\.scss$/, 
+            loader: [
+              MiniCSSExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+            ]
+          },
+        ]
+      },
+    plugins: [
+      new HtmlWebpackPlugin({  
+        filename: 'index.html',
+        template: './index.html',
+        hash: true
+      }),
+      new HtmlWebpackPlugin({  
+        filename: 'index.html',
+        template: './index.html',
+        hash: true
+      }),
+      new MiniCSSExtractPlugin()
+    ],
 }
