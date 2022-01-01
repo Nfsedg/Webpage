588a2ced4ff6c758e59c88f738874acdd935d116
Edgar Pech <edgaropech@hotmail.com>
2021-12-31 20:54:01 -0500

    [add] modals added for each proyects

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,18 +1,30 @@
-const proyects = document.querySelector(".proyects__info--timetrackig")
-const modals = document.querySelector(".modal")
-const button = document.querySelector(".button")
+const proyects = document.querySelectorAll(".proyects__info")
 
-proyects.addEventListener('click', openModal)
+proyects.forEach(element => {
+    element.addEventListener('click', () => {
+        openModal(element.id)
+    })
+    
+});
+// In HTML document the first node on the page on My Proyects must be the image and title displayed on the webpage
+function openModal(id) {
+    let modalArray = document.querySelectorAll(`#${id}`)
+    let modal = modalArray[1]
 
-function openModal() {
-    modals.classList.remove('hideModal')
-    modals.classList.add('show-modal')
+    console.log(modal)
+    let localButton = modal.childNodes[1].childNodes[1]
+    localButton.addEventListener('click', () => closeModal(id))
+
+    if(modal) {
+        modal.classList.remove('hideModal')
+        modal.classList.add('show-modal')
+    } else { 
+        console.warn("Modal doesn't exist")
+    }
 }
 
-
-button.addEventListener('click', closeModal)
-
-function closeModal() {
-    modals.classList.remove('show-modal')
-    modals.classList.add('hideModal')
+function closeModal(id) {
+    let modal = document.querySelectorAll(`#${id}`)
+    modal[1].classList.remove('show-modal')
+    modal[1].classList.add('hideModal')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,45 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./docs/scripts/queryjs.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "main.js",
-    },
-    module: {
-        rules: [
+  entry: "./docs/scripts/queryjs.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
           {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ['@babel/preset-env']
-              }
+            loader: "sass-loader",
+            options: {
+              includePaths: [path.resolve(__dirname, './node_modules')]
             }
-          },
-          { 
-            test: /\.s[ac]ss$/i, 
-            use: [
-              MiniCSSExtractPlugin.loader,
-              "css-loader",
-              "sass-loader",
-              {
-                loader: "sass-loader",
-                options: {
-                  includePaths: [path.resolve(__dirname, './node_modules')]
-                }
-              }
-            ]
           }
         ]
-      },
-    plugins: [
-      new HtmlWebpackPlugin({  
-        filename: 'index.html',
-        template: './index.html',
-        hash: true
-      }),
-      new HtmlWebpackPlugin({  
-        filename: 'index.html',
-        template: './index.html',
-        hash: true
-      }),
-      new MiniCSSExtractPlugin()
-    ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      hash: true
+    }),
+    new MiniCSSExtractPlugin()
+  ],
 }
